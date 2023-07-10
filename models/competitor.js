f7942eb4a472872415813741607a2b1352ad6e47
mckolles <koless98@icloud.com>
2023-07-10 17:16:05 +0300

    Победа братцы

--- models/competitor.js
+++ models/competitor.js
@@ -1,33 +1,33 @@
-'use strict'
+const { Sequelize, DataTypes } = require('sequelize');
+const { sequelize } = require('../db');
 
-module.exports=(sequelize, DataTypes)=>{
-  const Competitor=sequelize.define("Competitor",{
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    created_at: {
-      type: DataTypes.DATE,
-      allowNull: false,
-      defaultValue: DataTypes.NOW,
-    },
-    updated_at: {
-      type: DataTypes.DATE,
-      allowNull: true,
-      defaultValue: DataTypes.NOW,
-    },
-    deleted_at: {
-      type: DataTypes.DATE,
-      allowNull: true,
-    },
+const Competitor = sequelize.define('competitor', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  created_at: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    defaultValue: DataTypes.NOW,
+  },
+  updated_at: {
+    type: DataTypes.DATE,
+    allowNull: true,
+    defaultValue: DataTypes.NOW,
+  },
+  deleted_at: {
+    type: DataTypes.DATE,
+    allowNull: true,
   }
-  )
+}, {
+  tableName: 'competitors',
+  timestamps: false,
+});
 
-  return Competitor
-}
-  
+module.exports = Competitor;
--- server.js
+++ server.js
@@ -1,51 +1,17 @@
 const express = require('express');
-const cors=require('cors')
-const { Sequelize } = require('sequelize');
-const Competitor = require('./models/competitor.js');
-
-
-const sequelize = new Sequelize('competitors', 'root', 'Koleso98', {
-  host: 'localhost',
-  dialect: 'mysql',
-});
+const apiRoutes=require('./routes.js')
+const {Sequelize, conectToDb}=require('./db.js')
+const cors = require('cors');
 
 
 
 const app = express();
-
-const corOptions={
-  oirigin:'http://localhost:3000/'
-}
-
-app.use(cors(corOptions))
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-
-app.get('/competitors', async (req, res) => {
-    console.log('Fetching competitors...');
-    const competitors = await Competitor.findAll();
-    res.send(competitors);
- 
-});
-
-app.get('/', (req, res) => {
-  res.send('Hello, server is running!');
-});
-
-sequelize.authenticate()
-  .then(() => {
-    console.log('Connection to the database has been established successfully.');
-    return sequelize.sync();
-  })
-  .then(() => {
-    console.log('All models were synchronized successfully.');
-  })
-  .catch((err) => {
-    console.error('Unable to connect to the database:', err);
-  });
+app.use(cors())
+app.use('/', apiRoutes);
 
 const PORT=process.env.PORT||4000
 
-app.listen(PORT, () => {  
-  console.log('Server is running on port 4000');
+app.listen(PORT, async () => {  
+  console.log('Server is running on port 4000')
+  await conectToDb( )
 });
